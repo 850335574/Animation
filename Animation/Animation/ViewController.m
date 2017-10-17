8e1e141bc4b6c62f5aad33678c0b031da78cc606
15393126602 <88888888>
2017-10-17 14:11:02 +0800

    提交：新建分支项目

---
+++ Animation/Animation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Animation
+//
+//  Created by 王光虎 on 17/10/17.
+//  Copyright © 2017年 gsww. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Animation/Animation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Animation
+//
+//  Created by 王光虎 on 17/10/17.
+//  Copyright © 2017年 gsww. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
